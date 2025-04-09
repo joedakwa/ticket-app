ace5dd574bfce5a363f3b13006175417b0708a25
joe <joedakwa@hotmail.co.uk>
2025-04-09 12:37:57 +0100

    updated nav.jsx with faHome icon

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,7 +1,20 @@
+import Link from "next/link"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons"
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  return <nav>
+    <div>
+        <Link href="/">
+        <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+        <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+    </div>
+    <div>
+        <p className="text-default-text">dave@gmail.com</p>
+    </div>
+  </nav>
+};
 
-export default Nav
+export default Nav;
