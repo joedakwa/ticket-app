6440e044a02b7ab6faf20ebb8fa1a15f477c35fc
joe <joedakwa@hotmail.co.uk>
2025-04-09 13:26:57 +0100

    resolved @layer component issue by replacing with standard CSS

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons"
+
 const Nav = () => {
-  return <nav>
-    <div>
-        <Link href="/">
+  return (<nav className="flex justify-between bg-nav p-4">
+    <div className="flex items-center space-x-4">
+        <Link href="/"> 
         <FontAwesomeIcon icon={faHome} className="icon" />
         </Link>
         <Link href="/TicketPage/new">
@@ -15,6 +16,7 @@ const Nav = () => {
         <p className="text-default-text">dave@gmail.com</p>
     </div>
   </nav>
+  );
 };
 
 export default Nav;
